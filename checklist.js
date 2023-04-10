a83d0d348653ce5c77864eef89115208d4d8f20e
opju21 <117085376+opju21@users.noreply.github.com>
2023-04-10 23:45:09 +0200

    im doin me best ;-;

--- checklist.js
+++ checklist.js
@@ -1,1 +1,17 @@
-
+const myNodelist = document.getElementsByTagName("LI");
+let i;
+for (i = 0; i < myNodelist.length; i++) {
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+function newItem() {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("addToList").value;
+  const t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  document.getElementById("list").appendChild(li);
+  document.getElementById("addToList").value = "";
+}
