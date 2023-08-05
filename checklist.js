577f80a3953ea96ccd484e8443d2c16aa1cd5442
opju21 <opju21vj@student.ju.se>
2023-08-05 10:10:00 +0200

    Added closing buttons to all the thingz

--- checklist.js
+++ checklist.js
@@ -1,13 +1,33 @@
-let input = document.getElementById("addInput");
+let input = document.getElementById("addInput"); // the input feild id made into a value
+let listed = document.getElementById("listedItems"); // makes the ul with id listedItems to a value
+
+function addCloseButton(newListItem) {
+  const closeButton = document.createElement("button"); // this creates an btton element
+
+  closeButton.className = "deleteItemClass"; //This gives it a class
+  closeButton.innerText = "X"; //this just makes the x in the button
+
+  closeButton.addEventListener("click", function () {
+    listed.removeChild(newListItem); //if button is pressed this takes away the element / listitem
+  });
+  newListItem.appendChild(closeButton); // connects the closebutton to the list item
+}
 
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
-    let listed = document.getElementById("listedItems");
-    let newListItem = document.createElement("li");
-    const addInput = document.getElementById("addInput").value;
-    const node = document.createTextNode(addInput);
-    newListItem.appendChild(node);
-    listed.appendChild(newListItem);
-    input.value = " ";
+    let newListItem = document.createElement("li"); // creates a list element
+    const addInput = document.getElementById("addInput").value; // Gets the value from the input feild
+    const node = document.createTextNode(addInput); // Create text from the value from the input feild
+    newListItem.appendChild(node); // puts the text just made into the list element
+    listed.appendChild(newListItem); // makes so the list element is in the ul element which is stated in the beginning
+    input.value = " "; // makes the input feild blank after use
+    addCloseButton(newListItem); //connects the list item to a close button
   }
 });
+
+let listItems = document.getElementsByTagName("li"); // looks att all the li elements
+
+for (let i = 0; i < listItems.length; i++) {
+  //adds closebuttons to existing elements
+  addCloseButton(listItems[i]);
+}
