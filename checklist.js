c405c0d1731b5319a64f5d8129097ff5aaf368ee
opju21 <opju21vj@student.ju.se>
2023-08-06 12:53:04 +0200

    ITS WORKING, now i have made the array

--- checklist.js
+++ checklist.js
@@ -1,13 +1,19 @@
+let listedTaskArray = [];
+let jsonStringListedTaskArray = JSON.stringify(listedTaskArray);
 let listed = document.getElementById("listedItems");
+
+let input = document.getElementById("addInput");
 
 // LIST FUNCTION
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     let list = document.createElement("li");
-    let input = document.getElementById("addInput").value;
-    let textNode = document.createTextNode(input);
+    let inputValue = input.value;
+    let textNode = document.createTextNode(inputValue);
     list.appendChild(textNode);
     listed.appendChild(list);
-    document.getElementById("addInput").value = "";
+    listedTaskArray.push(inputValue);
+    localStorage.setItem("todoList", JSON.stringify(listedTaskArray));
+    input.value = "";
   }
 });
