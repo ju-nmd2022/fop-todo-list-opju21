0b79ddbe985ee6900be8f6e5b758031303938101
opju21 <117085376+opju21@users.noreply.github.com>
2023-08-06 11:54:17 +0200

    why does it not work D:

--- checklist.js
+++ checklist.js
@@ -1,12 +1,7 @@
 let listedTasks = [];
-let listedTasksJsonString = JSON.stringify(listedTasks);
-localStorage.setItem("list", listedTasksJsonString);
-
 let listed = document.getElementById("listedItems"); // makes the ul with id listedItems to a value
 
-// ADD CLOSE BUTTON FUNCTION
-
-// ADD TO LIST FUNCTION
+// LIST FUNCTION
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     let newListItem = document.createElement("li"); // creates a list element
@@ -14,9 +9,10 @@ input.addEventListener("keypress", function (event) {
     const node = document.createTextNode(addInput); // Create text from the value from the input feild
     newListItem.appendChild(node); // puts the text just made into the list element
     listed.appendChild(newListItem); // makes so the list element is in the ul element which is stated in the beginning
+
+    //listedTasks.push(addInput);
+    //localStorage.setItem("list", JSON.stringify(listedTasks));
+
     input.value = " "; // makes the input feild blank after use
   }
 });
-localStorage.setItem("list", listedTasksJsonString);
-
-// CHECK MARK FUNCTION
